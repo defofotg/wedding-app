df6a0e2c7a53b9bcfe6e91ee5b93aa031f30b1fa
Georges DEFO <georges.defo-fotsi-ext@rtone.fr>
2023-03-15 00:21:59 +0100

    conteneurisation wip

--- src/test/java/com/enai/wedding/WeddingApplicationTests.java
+++ src/test/java/com/enai/wedding/WeddingApplicationTests.java
@@ -2,8 +2,10 @@ package com.enai.wedding;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class WeddingApplicationTests {
 
 	@Test
